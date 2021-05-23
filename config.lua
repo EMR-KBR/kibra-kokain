6b2dfac17f5a925b034cc370d024aa1c43071cb0
kibra <62299912+EMR-KBR@users.noreply.github.com>
2021-05-22 18:02:52 -0700

    kibra-kokain

---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+
+-- Kordinat
+Config.KokainTopla  = {x = 1125.38, y= -1231.3, z = 16.4185}
+Config.KokainAyristir = {x = 1120.95, y = -1234.8, z = 16.4185}
+Config.KokainKurutma = {x = 1119.92, y = -1233.4, z = 16.4185}
+Config.KokainPaketleme = {x = 1116.89, y = -1239.5, z = 16.4185}
+
+--Satıcı
+Config.Satici = {x = 751.791, y = 6458.68, z =  30.5270}
+
+Config.Para = 30000 -- 5 Tanesi için
+
+-- Info
+Config.KokainBlip = true -- true olması durumunda Kokain blibi aktif hale gelir.
+Config.KaraPara = true --- True olması durumunda karapara verir. Aksi taktirde nakit verir.
+Config.SaticiBlip = true -- false olunca Satıcı blibini kapatır
+
+
+
