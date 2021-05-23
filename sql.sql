9801aeb55d0e54daab50abafdcd654a15cd806d6
kibra <62299912+EMR-KBR@users.noreply.github.com>
2021-05-23 04:01:16 +0300

    Delete sql.sql

--- sql.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `weight`, `limit`, `rare`, `can_remove`) VALUES
-('hamkokain', 'Ham Kokain', 0, 100, 0, 1),
-('ayristirilmis_kokain', 'Ayrıştırılmış Kokain', 1, 100, 0, 1),
-('kurutulmus_kokain', 'Kurutulmuş Kokain', 0.5, 100, 0, 1),
