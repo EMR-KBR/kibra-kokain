8a7859552c57888658c93a1084feffd8da5130b2
kibra <62299912+kibradev@users.noreply.github.com>
2021-05-23 17:19:25 +0300

    fixed

--- sql/sql.sql
+++ sql/sql.sql
@@ -2,3 +2,5 @@ INSERT INTO `items` (`name`, `label`, `weight`, `limit`, `rare`, `can_remove`) V
 ('hamkokain', 'Ham Kokain', 0, 100, 0, 1),
 ('ayristirilmis_kokain', 'Ayrıştırılmış Kokain', 1, 100, 0, 1),
 ('kurutulmus_kokain', 'Kurutulmuş Kokain', 0.5, 100, 0, 1),
+('paketlenmis_kokain', 'Kurutulmuş Kokain', 0.5, 100, 0, 1)
+;
